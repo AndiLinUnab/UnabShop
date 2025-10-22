2b1238b1455af3ff65b6480dfb24603225ae51a4
Andi Lin <alin@unab.edu.co>
2025-10-22 09:51:14 -0500

    Login and Register for UnabShop

--- app/src/main/java/me/andilin/unabshop/HomeScreen.kt
+++ app/src/main/java/me/andilin/unabshop/HomeScreen.kt
@@ -1,77 +1,43 @@
-package me.andilin.unabshop
+package me.andilin.loginandregisterfirebaseauth
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MediumTopAppBar
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
-@Preview
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
-    Scaffold(
-        topBar = {
-            MediumTopAppBar(
-                title = {
-                    Text(
-                        "Unab Shop",
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 28.sp
-                    )
-                },
-                actions = {
-                    IconButton(onClick = { }) {
-                        Icon(Icons.Filled.Notifications, "Notificaciones")
-                    }
-                    IconButton(onClick = { }) {
-                        Icon(Icons.Filled.ShoppingCart, "Carrito")
-                    }
-                    IconButton(onClick = { }) {
-                        Icon(Icons.AutoMirrored.Filled.ExitToApp, "Carrito")
-                    }
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = Color(0xFFFF9900),
-                    titleContentColor = Color.White,
-                    actionIconContentColor = Color.White
+fun HomeScreen(onClickLogout: () -> Unit = {}) {
+    val auth = Firebase.auth
+    val user = auth.currentUser
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column {
+            Text("HOME SCREEN", fontSize = 30.sp)
+
+            if(user != null) {
+                Text(user.email.toString())
+            } else {
+                Text("No hay usuario")
+            }
+            Button(onClick = {
+                auth.signOut()
+                onClickLogout()
+            },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFF9900)
                 )
-            )
-        },
-        bottomBar = {
-        }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFFF5F5F5))
-                .padding(paddingValues)
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("HOME SCREEN", fontSize = 30.sp)
+                Text("Cerrar Sesión")
             }
         }
     }
--- app/src/main/java/me/andilin/unabshop/MainActivity.kt
+++ app/src/main/java/me/andilin/unabshop/MainActivity.kt
@@ -1,4 +1,4 @@
-package me.andilin.unabshop
+package me.andilin.loginandregisterfirebaseauth
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,33 +9,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import me.andilin.unabshop.ui.theme.UnabShopTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
-            val startDestination = "login"
-            NavHost(navController, startDestination) {
-                composable(route = "login") {
-                    LoginScreen()
-                }
-                composable(route = "register") {
-                    RegisterScreen()
-                }
-                composable(route = "home") {
-                    HomeScreen()
-                }
-
-            }
+                NavigationApp()
         }
     }
 }
